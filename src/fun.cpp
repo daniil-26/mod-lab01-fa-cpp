9dd1da3fa12068fa8394ae3642dcda85d11089ee
daniil-26 <126852384+daniil-26@users.noreply.github.com>
2023-03-12 00:43:56 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -2,19 +2,19 @@
 #include "fun.h"
 
 unsigned int faStr1(const char *str) {
-    unsigned int count=0;
-    bool word=false, correct=false;
-    for(int i=0; str[i]!='\0'; i++) {
-        if(str[i]!=' ') {
+    unsigned int count = 0;
+    bool word = false, correct = false;
+    for(int i = 0; str[i] != '\0'; i++) {
+        if(str[i] != ' ') {
             if(!word) {
-                word=true;
-                correct=true;
+                word = true;
+                correct = true;
             }
             if(word && correct && isdigit(str[i]))
-                correct=false;
+                correct = false;
         }
         else if(word) {
-            word=false;
+            word = false;
             if(correct)
                 count++;
         }
@@ -25,26 +25,26 @@ unsigned int faStr1(const char *str) {
 }
 
 unsigned int faStr2(const char *str) {
-    unsigned int count=0;
-    bool word=false, correct=false, first=false;
-    for(int i=0; str[i]!='\0'; i++) {
-        if(str[i]!=' ') {
+    unsigned int count = 0;
+    bool word = false, correct = false, first = false;
+    for(int i = 0; str[i] != '\0'; i++) {
+        if(str[i] != ' ') {
             if(!word) {
-                word=true;
-                if('A'<=str[i] && str[i]<='Z') {
-                    correct=true;
-                    first=true;
+                word = true;
+                if('A' <= str[i] && str[i] <= 'Z') {
+                    correct = true;
+                    first = true;
                 }
             }
-            else first=false;
-            if(word && correct && !first && !('a'<=str[i] && str[i]<='z'))
-                correct=false;
+            else first = false;
+            if(word && correct && !first && !('a' <= str[i] && str[i] <= 'z'))
+                correct = false;
         }
         else if(word) {
-            word=false;
+            word = false;
             if(correct) {
                 count++;
-                correct=false;
+                correct = false;
             }
         }
     }
@@ -54,23 +54,23 @@ unsigned int faStr2(const char *str) {
 }
 
 unsigned int faStr3(const char *str) {
-    unsigned int count=0, length=0, sum=0;
-    bool word=false;
-    for(int i=0; str[i]!='\0'; i++) {
-        if(str[i]!=' ') {
-            word=true;
+    unsigned int count = 0, length = 0, sum = 0;
+    bool word = false;
+    for(int i = 0; str[i] != '\0'; i++) {
+        if(str[i] != ' ') {
+            word = true;
             length++;
         }
         else if(word) {
-            word=false;
-            sum+=length;
-            length=0;
+            word = false;
+            sum += length;
+            length = 0;
             count++;
         }
     }
     if(word) {
-        sum+=length;
+        sum += length;
         count++;
     }
-    return round((float)sum/count);
+    return round((float)sum / count);
 }
