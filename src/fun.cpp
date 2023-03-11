1ebbbee711a5fbfe3b93b8234c535f71d7e0d181
daniil-26 <126852384+daniil-26@users.noreply.github.com>
2023-03-12 00:30:13 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -2,13 +2,75 @@
 #include "fun.h"
 
 unsigned int faStr1(const char *str) {
-    return 0;
+    unsigned int count=0;
+    bool word=false, correct=false;
+    for(int i=0; str[i]!='\0'; i++) {
+        if(str[i]!=' ') {
+            if(!word) {
+                word=true;
+                correct=true;
+            }
+            if(word && correct && isdigit(str[i]))
+                correct=false;
+        }
+        else if(word) {
+            word=false;
+            if(correct)
+                count++;
+        }
+    }
+    if(word && correct)
+        count++;
+    return count;
 }
 
 unsigned int faStr2(const char *str) {
-    return 0;
+    unsigned int count=0;
+    bool word=false, correct=false, first=false;
+    for(int i=0; str[i]!='\0'; i++) {
+        if(str[i]!=' ') {
+            if(!word) {
+                word=true;
+                if('A'<=str[i] && str[i]<='Z') {
+                    correct=true;
+                    first=true;
+                }
+            }
+            else first=false;
+            if(word && correct && !first && !('a'<=str[i] && str[i]<='z'))
+                correct=false;
+        }
+        else if(word) {
+            word=false;
+            if(correct) {
+                count++;
+                correct=false;
+            }
+        }
+    }
+    if(word && correct)
+        count++;
+    return count;
 }
 
 unsigned int faStr3(const char *str) {
-    return 0;
+    unsigned int count=0, length=0, sum=0;
+    bool word=false;
+    for(int i=0; str[i]!='\0'; i++) {
+        if(str[i]!=' ') {
+            word=true;
+            length++;
+        }
+        else if(word) {
+            word=false;
+            sum+=length;
+            length=0;
+            count++;
+        }
+    }
+    if(word) {
+        sum+=length;
+        count++;
+    }
+    return round((float)sum/count);
 }
