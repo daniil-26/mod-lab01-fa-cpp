f24b1a5ee9097d03d59b0f83657f6aa250495034
daniil-26 <126852384+daniil-26@users.noreply.github.com>
2023-03-12 17:57:59 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,11 @@
 int main() {
     const char * str = " Hello world     1000";
     std::cout << "string: " << str << "\n"
-    << "\nwithout numbers : " << faStr1(str) << " words"
-    << "\nlatin letters, first capital, then lowercase : " << faStr2(str) << " words"
-    << "\naverage word length : " << faStr3(str) << " characters";
+    << "\nwithout numbers : "
+    << faStr1(str) << " words"
+    << "\nlatin letters, first capital, then lowercase : "
+    << faStr2(str) << " words"
+    << "\naverage word length : "
+    << faStr3(str) << " characters";
     return 0;
 }
