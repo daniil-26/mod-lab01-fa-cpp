a59122387b1abfd741619a9cea9781d924f39e01
daniil-26 <126852384+daniil-26@users.noreply.github.com>
2023-03-12 17:51:52 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,9 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
-#include <iostream>
-using namespace std;
 
-int main () {
+int main() {
     const char * str = " Hello world     1000";
-    cout << "string: " << str << "\n"
+    std::cout << "string: " << str << "\n"
     << "\nwithout numbers : " << faStr1(str) << " words"
     << "\nlatin letters, first capital, then lowercase : " << faStr2(str) << " words"
     << "\naverage word length : " << faStr3(str) << " characters";
